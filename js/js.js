8aaac633b8cc662377a36dc8ada5c6c7d9a4614a
agutierrez <agutierrez-2021188@kinal.edu.gt>
2024-03-21 17:56:29 -0600

    PROYECTO "FLORES AMARILLAS", FINALIZADO

--- js/js.js
+++ js/js.js
@@ -1,5 +1,7 @@
 
 onload = () =>{
+  alert("🌟 Para: \n  ㅤㅤㅤ 🌷 Mi Persona Favorita En Todo El Universoㅤ💞")
+  alert("ㅤ❤  Te amoo mas que a nada en el mundo  ❤ \n ㅤㅤㅤ🌷 Conmigo tu nunca seras expectador mi amor ❣\n ㅤㅤㅤ🌷 Espero y te gusten ✨\nㅤㅤㅤ 🌷 Att: El niñoo que mas TE AMA en el mundo ❤✨");
   document.body.classList.remove("container");
 };
   /*1000*/
